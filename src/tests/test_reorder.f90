ee7d8964b50eada8555e928473173ff1d8760b10
felipe <tifonzafel@gmail.com>
2019-03-25 13:11:12 +0100

    test sorted matrix

--- src/tests/test_reorder.f90
+++ src/tests/test_reorder.f90
@@ -1,27 +1,25 @@
 program main
-  
+
   use array_utils, only: sort_symmetric_matrix, diagonal, norm
   use davidson, only: generalized_eigensolver
   use numeric_kinds, only: dp
   use test_utils, only: read_matrix
 
-
   implicit none
- 
+
   integer, parameter :: dim = 864
   integer, parameter :: lowest = 6
-  real(dp), dimension(lowest) :: eigenvalues_DPR
+  real(dp), dimension(lowest) :: eigenvalues_DPR, expected
   real(dp), dimension(dim, lowest) :: eigenvectors_DPR
   real(dp), dimension(dim, dim) :: mtx
   integer :: iter_i
 
-  ! mtx = read_matrix("/home/felipe/Primer/Fortran_Davidson/src/tests/data/bse_singlet.dat", dim)
-  mtx = read_matrix("/home/felipe/Primer/Fortran_Davidson/src/tests/data/sorted_bse.dat", dim)
-
-  ! call sort_symmetric_matrix(mtx)
+  mtx = read_matrix("../src/tests/data/sorted_bse.dat", dim)
 
   call generalized_eigensolver(mtx, eigenvalues_DPR, eigenvectors_DPR, lowest, "DPR", 15, 1d-4, iter_i, lowest*3)
 
-  print *, eigenvalues_DPR
-  
+  expected = [0.30445426, 0.31341032, 0.31360998, 0.33246853, 0.34212415, 0.35761287]
+
+  print *, "Error expected eigenvalues: ", norm(eigenvalues_DPR - expected)
+
 end program main
